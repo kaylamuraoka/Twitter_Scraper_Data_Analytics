213d7b43bc0ff31f620e9f7801b1eadb67216eb6
kaylamuraoka <kmurs98@gmail.com>
2020-10-15 10:19:26 -1000

    uploading twitter stramer and file to grap the most popular tweets

---
+++ get_popular_tweets.py
@@ -0,0 +1,77 @@
+# import necessary libraries
+import requests as rq
+from requests_oauthlib import OAuth1
+import pandas as pd
+import time
+
+# prompt the user for a word
+query = input('What word/hashtag should we search for? (Enter a single word): ').strip()
+
+# check that the user input is a valid string
+try:
+    str(query)
+    pass
+
+except ValueError:
+    print("Sorry, that is not a valid word, perhaps you mistyped?")
+    query = input('Try again, what word/hashtag should we search for? (Enter a single word): ').strip()
+
+# The function by default has the result_type = 'popular'
+def get_most_popular_twitter_tweets(query, result_type = 'popular'):
+
+    print("Searching for tweets within the last week that mention " + query + '\n')
+    # Define API Key, Search Type, and authorization
+    twitter_search_path = 'https://api.twitter.com/1.1/search/tweets.json'
+
+    auth = OAuth1 ('U57SZ7qXdxFgFIrfdn6E2yk1U',
+                   'fVSW996YZyTlJdA4xvRSVWne9hSRdPBTrnQRnKGhr2pGZchlzB',
+                   '765282870172803072-u90XOV6lFK6sN6MiDUqbml8yVAmSjiF',
+                   'ONCkmYBLH71EHJpiOoE7uFxMzbE47B6bcIq0Y8roRKlcw'
+     )
+
+    # Define the Parameters of the search
+    search_params = {
+        'q': [query, '#' + query],
+        'lang':'en',
+        'result_type': result_type, # Specifies what type of search results you would prefer to receive.
+        'count': 15,  # The number of tweets to return per page, up to a maximum of 15.
+        
+    }
+
+    # Make a Request to the API, and return results
+    # Convert response to a JSON String
+    response = rq.get(twitter_search_path, params=search_params, auth=auth).json()
+    
+    # Calling DataFrame constructor 
+    df = pd.DataFrame([])
+    dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': [], 'retweet_count': [], 'hashtags': []}
+
+    count = 0
+    
+    for tweet in response['statuses']:
+        count += 1
+        if count <= 15 and 'RT @' not in tweet['text']:
+ 
+            dict_['user'].append(tweet['user']['screen_name'])
+            dict_['date'].append(tweet['created_at'])
+            dict_['text'].append(tweet['text'])
+            dict_['favorite_count'].append(tweet['favorite_count'])
+            dict_['retweet_count'].append(tweet['retweet_count'])
+            hashtags = tweet['entities']['hashtags']
+            for hashtag in hashtags:
+                hashtags = hashtag['text']
+            dict_['hashtags'].append(hashtags)  
+            
+            print(str(count) + ':', tweet['text'], '\n')
+                
+    # Structure data in a pandas DataFrame for easier manipulation
+    df = pd.DataFrame(dict_)
+    
+    df.sort_values(by='favorite_count', inplace=True, ascending=False)
+    
+    # export df to a csv file in the same directory
+    df.to_csv ('trending_' + query + '_at_' + time.strftime('%Y-%m-%d-%H-%M') + '.csv', index = False, header=True)
+
+    print("Tweets written on: trending_" + query + '_at_' + time.strftime('%Y-%m-%d-%H-%M') + '.csv')
+
+most_popular_Tweets=get_most_popular_twitter_tweets(query, result_type = 'popular')
---
+++ tweet_stream.py
@@ -0,0 +1,120 @@
+# Setup necessary packages
+from __future__ import print_function
+import sys
+import tweepy
+from configparser import ConfigParser
+import time
+import csv
+
+# prompt the user for a word
+query = input('What word/hashtag should we search for? (Enter a single word): ').strip()
+
+# check that the user input is a valid string
+try:
+    str(query)
+    pass
+
+except ValueError:
+    print("Sorry, that is not a valid word, perhaps you mistyped?")
+    query = input('Try again, what word/hashtag should we search for? (Enter a single word): ').strip()
+
+#override tweepy.StreamListener to add logic to on_status
+class MyStreamListener(tweepy.StreamListener):
+
+    def __init__(self):
+        super().__init__()
+        self.itera = 0
+        # Create a file with user's input and  and the current time to save the tweets to
+        self.filename = query + 'tweets' + '_' + time.strftime('%Y-%m-%d-%H-%M-%S') + '.csv'
+        # Create a new file with that filename
+        csvFile = open(self.filename, 'w')
+        # Create a csv writer
+        writer = csv.writer(csvFile)
+        # Write a single row with the headers of the columns
+        writer.writerow(['num',
+                        'text',
+                        'hashtags',
+                        'date',
+                        'user',
+                        'users number of followers',
+                        'location'
+                        ])
+
+    # when tweet appears
+    def on_status(self, status):
+         
+        # Open the csv file created previously
+        csvFile = open(self.filename, 'a')
+
+        # Create a csv writer
+        writer = csv.writer(csvFile)
+        
+        if status.truncated:
+            text = status.extended_tweet['full_text'].replace('\n', ' ') + '\n'
+        else:
+            text = status.text.replace('\n', ' ') + '\n'
+        
+        hashtags = []   #make an empty list
+        for hashtag in status.entities['hashtags']:    #iterate over the list
+            hashtags.append(hashtag["text"])     #append each hashtag to 'hashtags'
+
+        # If the tweet is not a retweet
+        if not 'RT @' in status.text:
+            try:
+                # Write the tweet's information to the csv file
+                writer.writerow([self.itera,
+                                text,
+                                hashtags,
+                                status.created_at,
+                                status.user.screen_name,
+                                status.user.followers_count,
+                                status.user.location])
+        
+            except Exception as e:
+                print(e)
+                pass
+
+        self.itera += 1
+        if self.itera % 100 == 0:
+            csvFile.close()
+            # Return nothing
+            return
+    
+    # When an error occurs
+    def on_error(self, status_code):
+        # Print the error code
+        print('Encountered error with status code:', status_code)
+        
+        # If the error code is 401, which is the error for bad credentials
+        if status_code == 401:
+            # End the stream
+            return False
+        
+
+if __name__ == "__main__":
+    start_run = time.time()
+    print("Writing tweets to: " + query + 'tweets' + '_' + time.strftime('%Y-%m-%d-%H-%M-%S') + '.csv')
+    print("Press CTRL + c to terminate...")
+    time.sleep(2)
+
+    config = ConfigParser()
+    # read configuration file containing consumer key + secret, and accesss token + secret
+    config.read('config.cfg')
+
+
+    auth = tweepy.OAuthHandler(config.get('twitter', 'consumer_key'),
+                               config.get('twitter', 'consumer_secret'))
+
+    auth.set_access_token(config.get('twitter', 'access_token'),
+                          config.get('twitter', 'access_token_secret'))
+
+    api = tweepy.API(auth)
+
+    myStreamListener = MyStreamListener()
+    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+    try:
+        myStream.filter(languages=["en"], track=[query, '#' + query])
+    except KeyboardInterrupt:
+        print('*****************************\nInterrupted')
+        sys.exit()
